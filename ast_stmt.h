fb461eacbbac0e62995cf28bba83992ecfabd652
Hongjiu Zhang <zhanghj@umich.edu>
2014-02-28 09:37:21 -0500

    Add declaration conflict check for Program
    Signed-off-by: Hongjiu Zhang <zhanghj@umich.edu>

--- ast_stmt.h
+++ ast_stmt.h
@@ -3,7 +3,7 @@
  * The Stmt class and its subclasses are used to represent
  * statements in the parse tree.  For each statment in the
  * language (for, if, return, etc.) there is a corresponding
- * node class for that construct. 
+ * node class for that construct.
  *
  * pp3: You will need to extend the Stmt classes to implement
  * semantic analysis for rules pertaining to statements.
@@ -19,98 +19,108 @@
 class Decl;
 class VarDecl;
 class Expr;
-  
+
 class Program : public Node
 {
-  protected:
-     List<Decl*> *decls;
-     
-  public:
-     Program(List<Decl*> *declList);
-     void Check();
+    private:
+        Hashtable<Node> *sym_;
+
+    protected:
+        List<Decl*> *decls;
+
+    public:
+        Program(List<Decl*> *declList);
+        void Check();
 };
 
 class Stmt : public Node
 {
-  public:
-     Stmt() : Node() {}
-     Stmt(yyltype loc) : Node(loc) {}
+    public:
+        Stmt() : Node()
+        {
+        }
+        Stmt(yyltype loc) : Node(loc)
+        {
+        }
 };
 
-class StmtBlock : public Stmt 
+class StmtBlock : public Stmt
 {
-  protected:
-    List<VarDecl*> *decls;
-    List<Stmt*> *stmts;
-    
-  public:
-    StmtBlock(List<VarDecl*> *variableDeclarations, List<Stmt*> *statements);
+    protected:
+        List<VarDecl*> *decls;
+        List<Stmt*> *stmts;
+
+    public:
+        StmtBlock(List<VarDecl*> *variableDeclarations,
+                  List<Stmt*> *statements);
 };
 
-  
+
 class ConditionalStmt : public Stmt
 {
-  protected:
-    Expr *test;
-    Stmt *body;
-  
-  public:
-    ConditionalStmt(Expr *testExpr, Stmt *body);
+    protected:
+        Expr *test;
+        Stmt *body;
+
+    public:
+        ConditionalStmt(Expr *testExpr, Stmt *body);
 };
 
-class LoopStmt : public ConditionalStmt 
+class LoopStmt : public ConditionalStmt
 {
-  public:
-    LoopStmt(Expr *testExpr, Stmt *body)
-            : ConditionalStmt(testExpr, body) {}
+    public:
+        LoopStmt(Expr *testExpr, Stmt *body)
+            : ConditionalStmt(testExpr, body)
+        {
+        }
 };
 
-class ForStmt : public LoopStmt 
+class ForStmt : public LoopStmt
 {
-  protected:
-    Expr *init, *step;
-  
-  public:
-    ForStmt(Expr *init, Expr *test, Expr *step, Stmt *body);
+    protected:
+        Expr *init, *step;
+
+    public:
+        ForStmt(Expr *init, Expr *test, Expr *step, Stmt *body);
 };
 
-class WhileStmt : public LoopStmt 
+class WhileStmt : public LoopStmt
 {
-  public:
-    WhileStmt(Expr *test, Stmt *body) : LoopStmt(test, body) {}
+    public:
+        WhileStmt(Expr *test, Stmt *body) : LoopStmt(test, body) {}
 };
 
-class IfStmt : public ConditionalStmt 
+class IfStmt : public ConditionalStmt
 {
-  protected:
-    Stmt *elseBody;
-  
-  public:
-    IfStmt(Expr *test, Stmt *thenBody, Stmt *elseBody);
+    protected:
+        Stmt *elseBody;
+
+    public:
+        IfStmt(Expr *test, Stmt *thenBody, Stmt *elseBody);
 };
 
-class BreakStmt : public Stmt 
+class BreakStmt : public Stmt
 {
-  public:
-    BreakStmt(yyltype loc) : Stmt(loc) {}
+    public:
+        BreakStmt(yyltype loc) : Stmt(loc) {}
 };
 
-class ReturnStmt : public Stmt  
+class ReturnStmt : public Stmt
 {
-  protected:
-    Expr *expr;
-  
-  public:
-    ReturnStmt(yyltype loc, Expr *expr);
+    protected:
+        Expr *expr;
+
+    public:
+        ReturnStmt(yyltype loc, Expr *expr);
 };
 
 class PrintStmt : public Stmt
 {
-  protected:
-    List<Expr*> *args;
-    
-  public:
-    PrintStmt(List<Expr*> *arguments);
+    protected:
+        List<Expr*> *args;
+
+    public:
+        PrintStmt(List<Expr*> *arguments);
 };
 
 
