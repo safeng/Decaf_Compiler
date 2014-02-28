67efb71679cead03002058367f560d6bbddd15bc
Hongjiu Zhang <zhanghj@umich.edu>
2014-02-28 18:05:25 -0500

    Add DoCheck() for type nodes
    Signed-off-by: Hongjiu Zhang <zhanghj@umich.edu>

--- ast_type.cc
+++ ast_type.cc
@@ -2,16 +2,18 @@
  * -----------------
  * Implementation of type node classes.
  */
+
+#include <string.h>
+
 #include "ast_type.h"
 #include "ast_decl.h"
-#include <string.h>
- 
+
 /* Class constants
  * ---------------
- * These are public constants for the built-in base types (int, double, etc.)
- * They can be accessed with the syntax Type::intType. This allows you to
- * directly access them and share the built-in types where needed rather that
- * creates lots of copies.
+ * These are public constants for the built-in base types (int, double,
+ * etc.) They can be accessed with the syntax Type::intType. This
+ * allows you to directly access them and share the built-in types
+ * where needed rather that creates lots of copies.
  */
 
 Type *Type::intType    = new Type("int");
@@ -20,20 +22,58 @@ Type *Type::voidType   = new Type("void");
 Type *Type::boolType   = new Type("bool");
 Type *Type::nullType   = new Type("null");
 Type *Type::stringType = new Type("string");
-Type *Type::errorType  = new Type("error"); 
+Type *Type::errorType  = new Type("error");
 
 Type::Type(const char *n) {
     Assert(n);
     typeName = strdup(n);
+
+    return;
+}
+
+Type::Type(yyltype loc) : Node(loc)
+{
+    return;
+}
+
+void Type::PrintToStream(std::ostream& out)
+{
+    out << typeName;
+
+    return;
+}
+
+std::ostream& operator<<(std::ostream& out, Type *t)
+{
+    t->PrintToStream(out);
+
+    return out;
+}
+
+bool Type::IsEquivalentTo(Type *other)
+{
+    return this == other;
 }
 
 
+void NamedType::DoCheck(void)
+{
+    this->GetClass(this->typeName);
 
-	
+    return;
+}
+
 NamedType::NamedType(Identifier *i) : Type(*i->GetLocation()) {
     Assert(i != NULL);
     (id=i)->SetParent(this);
-} 
+}
+
+void NamedType::PrintToStream(std::ostream& out)
+{
+    out << id;
+
+    return;
+}
 
 
 ArrayType::ArrayType(yyltype loc, Type *et) : Type(loc) {
@@ -41,4 +81,9 @@ ArrayType::ArrayType(yyltype loc, Type *et) : Type(loc) {
     (elemType=et)->SetParent(this);
 }
 
+void ArrayType::PrintToStream(std::ostream& out)
+{
+    out << elemType << "[]";
 
+    return;
+}
--- ast_type.h
+++ ast_type.h
@@ -3,58 +3,63 @@
  * In our parse tree, Type nodes are used to represent and
  * store type information. The base Type class is used
  * for built-in types, the NamedType for classes and interfaces,
- * and the ArrayType for arrays of other types.  
+ * and the ArrayType for arrays of other types.
  *
  * pp3: You will need to extend the Type classes to implement
  * the type system and rules for type equivalency and compatibility.
  */
- 
+
 #ifndef _H_ast_type
 #define _H_ast_type
 
-#include "ast.h"
-#include "list.h"
 #include <iostream>
 
+#include "ast.h"
+#include "list.h"
 
-class Type : public Node 
+class Type : public Node
 {
-  protected:
-    char *typeName;
+    protected:
+        char *typeName;
 
-  public :
-    static Type *intType, *doubleType, *boolType, *voidType,
-                *nullType, *stringType, *errorType;
+    public :
+        static Type *intType;
+        static Type *doubleType;
+        static Type *boolType;
+        static Type *voidType;
+        static Type *nullType;
+        static Type *stringType;
+        static Type *errorType;
 
-    Type(yyltype loc) : Node(loc) {}
-    Type(const char *str);
-    
-    virtual void PrintToStream(std::ostream& out) { out << typeName; }
-    friend std::ostream& operator<<(std::ostream& out, Type *t) { t->PrintToStream(out); return out; }
-    virtual bool IsEquivalentTo(Type *other) { return this == other; }
+        Type(const char *str);
+        Type(yyltype loc);
+
+        virtual void PrintToStream(std::ostream& out);
+        friend std::ostream& operator<<(std::ostream& out, Type *t);
+        virtual bool IsEquivalentTo(Type *other);
 };
 
-class NamedType : public Type 
+class NamedType : public Type
 {
-  protected:
-    Identifier *id;
-    
-  public:
-    NamedType(Identifier *i);
-    
-    void PrintToStream(std::ostream& out) { out << id; }
+    protected:
+        Identifier *id;
+        void DoCheck(void);
+
+    public:
+        NamedType(Identifier *i);
+
+        void PrintToStream(std::ostream& out);
 };
 
-class ArrayType : public Type 
+class ArrayType : public Type
 {
-  protected:
-    Type *elemType;
+    protected:
+        Type *elemType;
 
-  public:
-    ArrayType(yyltype loc, Type *elemType);
-    
-    void PrintToStream(std::ostream& out) { out << elemType << "[]"; }
+    public:
+        ArrayType(yyltype loc, Type *elemType);
+
+        void PrintToStream(std::ostream& out);
 };
 
- 
 #endif
